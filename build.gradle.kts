09a9ac2db28fa9d6f34b1516a36f87faa275cd50
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2021-08-25 17:43:23 +0200

    build: upgrade some dependencies and update NOTICE.md files

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,9 @@ repositories {
 }
 dependencies {
     implementation("org.eclipse.keyple:keyple-util-java-lib:2.0.0-SNAPSHOT") { isChanging = true }
-    implementation("com.google.code.gson:gson:2.8.5")
-    implementation("org.slf4j:slf4j-api:1.7.25")
-    testImplementation("org.slf4j:slf4j-simple:1.7.25")
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    testImplementation("org.slf4j:slf4j-simple:1.7.32")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.assertj:assertj-core:3.15.0")
     testImplementation("org.mockito:mockito-core:2.28.2")
